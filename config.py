6d1443761565e531acce5be763c865250ac199d1
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-24 13:56:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,44 +1,35 @@
-# (c) @RknDeveloperr
-# Rkn Developer 
-# Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
-# Developer @RknDeveloperr
+import re
+import os
+import time
 
-import re, os, time
 id_pattern = re.compile(r'^.\d+$') 
 
 class Rkn_Bots(object):
     # Rkn client config
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = os.environ.get("API_ID", "23990433")
+    API_HASH = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5918318460:AAHBrhLDzSRtEhC9bG-PLBFdZ3eWnEH7fgU")
 
-    #start_pic
-    RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
+    # start_pic
+    RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/462b0bbd3623b54672f75.jpg")
 
-
-    # wes response configuration
+    # web response configuration
     BOT_UPTIME = time.time()
     PORT = int(os.environ.get("PORT", "8080"))
-    FORCE_SUB = os.environ.get("FORCE_SUB", "") 
-    
-    # database config
-    DB_NAME = os.environ.get("DB_NAME", "AutoCaption_V05_Bot")     
-    DB_URL = os.environ.get("DB_URL", "")
+    FORCE_SUB = os.environ.get("FORCE_SUB", "SK_MoviesOffl") 
 
-    #caption
-    DEF_CAP = os.environ.get("DEF_CAP",
-                             "<b><a href='telegram.me/Rkn_Bots'>{file_name} Telegram : @RknDeveloper\n\nForward the file before Downloading.</a></b>",
+    # database config
+    DB_NAME = os.environ.get("DB_NAME", "AutoCaption_Bot")     
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://sankar:sankar@sankar.lldcdsx.mongodb.net/?retryWrites=true&w=majority")
+
+    # caption
+    DEF_CAP = os.environ.get(
+        "DEF_CAP",
+        "<b><a href='telegram.me/Rkn_Bots'>{file_name} Telegram : @RknDeveloper\n\nForward the file before Downloading.</a></b>"
     )
 
-    #sticker Id
+    # sticker Id
     STICKER_ID = os.environ.get("STICKER_ID", "CAACAgIAAxkBAAELFqBllhB70i13m-woXeIWDXU6BD2j7wAC9gcAAkb7rAR7xdjVOS5ziTQE")
 
-    #admin id
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    
-
-# Rkn Developer 
-# Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
-# Developer @RknDeveloperr
+    # admin id
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5821871362').split()]
